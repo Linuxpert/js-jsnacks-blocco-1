7f92ab9bb7061f12edc430a71f76bed27253972f
Linuxpert <g.manfre10@gmail.com>
2021-10-28 17:53:25 +0200

    ultima prova sestro esercizio

--- jsnack6/script.js
+++ jsnack6/script.js
@@ -2,16 +2,21 @@
 
 //creare un arrey vuoto
 const randomNum = [];
-
+let found = false;
 //generare 50 numeri random
 while(randomNum.length < 50){
     let num = Math.floor(Math.random() * 100) +1;
-    
-    if(num !== randomNum.length){
-        randomNum.push(num);
+    // randomNum.push(num)
+    if(num !== randomNum){
+        found = true;
     }
     
 }
+
+if(found === true){
+    randomNum.push(num)
+}
+
 console.log(randomNum);
 
 //inserire nell'arrey i numeri non replicati
