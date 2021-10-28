bdcf96a11127c489a922091d2b3b4a34f8c74fa5
Linuxpert <g.manfre10@gmail.com>
2021-10-28 15:32:03 +0200

    terzo esercizio da rivedere

---
+++ jsnack3/script.js
@@ -0,0 +1,19 @@
+// Il software deve chiedere per 10 volte all’utente di inserire un numero.
+// Il programma stampa la somma di tutti i numeri inseriti.
+const result = document.getElementById("result");
+
+let sum = 0;
+//chiedi 10 volte un numero all'utente
+for(let i = 0; i < 10; i++){
+    let Num = prompt("scrivi un numero");
+    console.log("numeri",Num);
+    arrNum.push(Num);
+    console.log("lista",arrNum);
+   
+}
+
+//somma tutte le variabili in uscita
+
+//stamp ail numero 
+
+
