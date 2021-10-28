5a3bd4f4b21a24d55ef02d6d055c9953852dedc8
Linuxpert <g.manfre10@gmail.com>
2021-10-28 15:43:24 +0200

    quarto esercizio completato

---
+++ jsnack4/script.js
@@ -0,0 +1,22 @@
+// In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.
+const result =document.getElementById("result");
+//crea una lista con dei nomi
+const listName = ["Mario", "Simone", "Elena", "Giuseppe"];
+
+let nameFind = false;
+//chiedi all'utente il proprio nome
+let nameUser = prompt("Scrivi il tuo nome");
+
+//controllare nella lista se il nome è presente
+for(let i = 0; i < listName.length; i++) {
+    if(nameUser === listName[i]) {
+        nameFind = true;
+    }
+}
+    //se si può partecipare
+if(nameFind === true){
+    result.innerHTML = "Sei invitato alla festa"
+}else {
+    result.innerHTML = "Non sei autorizzato a stare qui"
+}
+    //se no non può partecipare
