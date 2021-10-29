7fc316ff87022726a8c986dfadac2cd49db76538
Linuxpert <g.manfre10@gmail.com>
2021-10-29 10:09:39 +0200

    piccole modifiche

--- jsnack4/script.js
+++ jsnack4/script.js
@@ -1,9 +1,9 @@
 // In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.
 const result =document.getElementById("result");
+let nameFind = false;
 //crea una lista con dei nomi
 const listName = ["Mario", "Simone", "Elena", "Giuseppe"];
 
-let nameFind = false;
 //chiedi all'utente il proprio nome
 let nameUser = prompt("Scrivi il tuo nome");
 
